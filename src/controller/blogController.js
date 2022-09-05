deb10f0ca294845a7117abdd0c7d3a481de81366
lalan992 <lalankumar992588@gmail.com>
2022-09-05 18:37:35 +0530

    updateblog_1

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -1,1 +1,22 @@
 const blogModel = require("../models/blogModel");
+
+const updateBlog = async function (req, res) {
+  try {
+    let id = req.params.blogId;
+    let blog = await blogModel.findById(id);
+    if (!blog && blog.isDeleted === true) {
+      res.status(404).send({
+        status: false,
+        msg: "",
+      });
+    }
+
+    let blog2 = await blogModel.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+  } catch (err) {
+    res.status(500).send({ msg: err.message });
+  }
+};
+
+module.exports.updateBlog = updateBlog;
