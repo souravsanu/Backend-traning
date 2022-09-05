c8ba30b611fbd0772e0f5416bb95338999b582f6
souravsanu <souravmondal01bscctis@gmail.com>
2022-09-05 17:11:41 +0530

    vnfjv

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -1,1 +1,7 @@
 const authorModel = require("../models/authorModel");
+
+const CreateAuthor = async function (req, res) {
+  let authorDetails = req.body;
+  let autorId = authorDetails.authorId;
+};
+module.exports.CreateAuthor = CreateAuthor;
