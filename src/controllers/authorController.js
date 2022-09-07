0772c88f484d90b5402c60b8f38c924e8eb76122
pradeep <pradeeppatil8379@gmail.com>
2022-09-07 14:09:22 +0530

    day3/checked edge cases

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,13 +3,34 @@ const authorModel = require("../models/authorModel")
 const createAuthor = async function (req, res) {
     try {
         let data = req.body
-        let checkEmail = data.email
-        let email = await authorModel.findOne({ email: checkEmail })
-        if (email) { return res.status(400).send({ status: false, msg: "Email is alredy exists" }) }
+        if(!data)
+        return res.status(400).send({ status: false, msg:  "enter data"  })
+        
+        let { fname, lname, title, email, password } = data
+        if (!fname) { return res.status(400).send({status:false, msg: "fname is required" }) }
+        if (!lname) { return res.status(400).send({status:false, msg: "lname is required" }) }
+        if (!title) { return res.status(400).send({ status:false,msg: "title is required" }) }
+        if (!email) { return res.status(400).send({ status:false,msg: "email is required" }) }
+        if (!password) { return res.status(400).send({status:false, msg: "password is required" }) }
+       
+        if (typeof fname !== "string" || fname[0]==" " || fname[fname.length-1] ==" ") {
+            return res.status(400).send({ status: false, msg: "please enetr a valid firstname" })
+        }
+        if (typeof lname !== "string" || lname[0]==" " || lname[lname.length-1] ==" ") {
+            return res.status(400).send({ status: false, msg: "please enetr a valid lastname" })
+        }
+        if (title !== "Mr" && title !== "Mrs" && title !== "Miss") {
+            return res.status(400).send({ status: false, msg: "please enter  Mr or Mrs or Miss" })
+        }
+        let findEmail = await authorModel.findOne({ email: email })
 
+        if (findEmail) {
+            return res.status(400).send({status:false, msg: "email id already exsits" })
+        }
+         fname.trim()
 
-        let savedData = await authorModel.create(data)
-        res.status(201).send({status: true, data: savedData })
+        let authorCreated = await authorModel.create(data)
+        res.status(201).send({status: true, data: authorCreated })
     } catch (error) {
         res.status(500).send({status: false, msg: error.message })
     }
@@ -17,3 +38,49 @@ const createAuthor = async function (req, res) {
 }
 
 module.exports.createAuthor = createAuthor
+
+
+
+
+
+// const creatAuthor = async function(req, res) {
+//     try {
+//         let data = req.body
+//         let { fname, lname, title, email, password } = data
+//         if (!fname) { return res.status(400).send({ msg: "fname is required" }) }
+//         if (!lname) { return res.status(400).send({ msg: "lname is required" }) }
+//         if (!title) { return res.status(400).send({ msg: "title is required" }) }
+//         if (!email) { return res.status(400).send({ msg: "email is required" }) }
+//         if (!password) { return res.status(400).send({ msg: "password is required" }) }
+//         //validation of corect insertionn 
+//         if (typeof fname !== "string" && fname.trim().length === 0) {
+//             return res.status(400).send({ status: false, msg: "please enetr a valid firstname" })
+//         }
+//         if (typeof lname !== "string" && fname.trim().length === 0) {
+//             return res.status(400).send({ status: false, msg: "please enetr a valid lastname" })
+//         }
+//         if (title !== "Mr" && title !== "Mrs" && title !== "Miss") {
+//             return res.status(400).send({ status: false, msg: "please enter  Mr or Mrs or Miss" })
+//         }
+//         let findEmail = await authorModel.findOne({ email: email })
+
+//         if (findEmail) {
+//             return res.status(400).send({ msg: "email id already exsits" })
+//         }
+//         let validEmail = function(mail) {
+//             return (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(mail))
+//         }
+//         if (!validEmail(email)) return res.status(400).send({ status: false, msg: "email is not valid" })
+//         let authorCreated = await authorModel.create(author)
+//         return res.status(201).send(authorCreated)
+//     } catch (error) {
+//         return res.status(500).send({ status: false, msg: error.message })
+//     }
+// }
+
+
+
+
+// let checkEmail = data.email
+//         let email = await authorModel.findOne({ email: checkEmail })
+//         if (email) { return res.status(400).send({ status: false, msg: "Email is alredy exists" }) }
--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,29 +1,33 @@
 const authorModel = require("../models/authorModel")
 const blogModel = require("../models/blogModel")
-
+let timeElapsed = Date.now();
+let today = new Date(timeElapsed);
 
 const createBlog = async function (req, res) {
     try {
         let data = req.body
+        if (!data)
+            return res.status(400).send({ status: false, msg: "enter data" })
         let AuthorId = data.authorId
         if (!AuthorId) return res.status(404).send({ status: false, msg: "AuthorId Missing" })
+        if (AuthorId.length > 24 || AuthorId.length < 24)
+            return res.status(400).send({ status: false, msg: "Invalid ID" })
 
         let correctAuthorId = await authorModel.findById(AuthorId)
         if (!correctAuthorId) return res.status(404).send({ status: false, msg: "AuthorId is not Valid" })
-        let timeElapsed = Date.now();
-        let today = new Date(timeElapsed);
+        
         if (data.isPublished) {
             data.publishedAt = today
         }
         if (data.deletedAt) {
             data.deletedAt = today
         }
-        console.log(data)
+
 
         let savedData = await blogModel.create(data)
-        res.status(201).send({status: true, data: savedData })
+        res.status(201).send({ status: true, data: savedData })
     } catch (error) {
-        res.status(500).send({ status: false,msg: error.message })
+        res.status(500).send({ status: false, msg: error.message })
     }
 
 }
@@ -32,11 +36,21 @@ const getBlogs = async function (req, res) {
 
     try {
         let filters = req.query;
+       
+        if (Object.keys(filters).length ==0) {
+            
+            let data = await blogModel.find({isDeleted:false,isPublished:true})
+            return res.status(200).send({ status: true, data: data })
+        }
         filters["isDeleted"] = false;
         filters["isPublished"] = true;
+        if (Object.keys(filters).length > 6) {
+            return res.status(400).send({ status: false, msg: "enter valid filters" })
+        }
+
         let data = await blogModel.findOne(filters).populate("authorId")
         if (!data) res.status(404).send({ status: false, msg: "file not found!" })
-        res.status(200).send({ status: true, data:  data  })
+        return res.status(200).send({ status: true, data: data })
 
     }
     catch (error) {
@@ -45,55 +59,61 @@ const getBlogs = async function (req, res) {
 }
 
 const updateBlog = async function (req, res) {
-    try{
-    let blogId = req.params.blogId;
-    if (!blogId)
-    return  res.status(404).send({ status: false, msg: "enter a blog ID" })
-    let valid = await blogModel.findById(blogId)
-    if (!valid)
-    return    res.status(404).send({ status: false, msg: "invalid blog ID" })
-    if (valid.isDeleted == true)
-    return   res.status(404).send({ status: false, msg: "no such blog exists" })
-    let timeElapsed = Date.now();
-    let today = new Date(timeElapsed);
-    let updates = req.body
-    if (!updates)
-    return res.status(400).send({ status: false, msg: "send something to update" })
-    let stringUpdates = {};
-    let arrayUpdates = {};
-    if (updates["title"])
-        stringUpdates["title"] = updates.title;
-    if (updates["body"])
-        stringUpdates["body"] = updates.body;
-    if (updates["tags"])
-        arrayUpdates["tags"] = updates.tags;
-    if (updates["subcategory"])
-        arrayUpdates["subcategory"] = updates.subcategory;
-    stringUpdates["isPublished"] = true;
-    stringUpdates["publishedAt"] = today;
-    let updatedBlog = await blogModel.findByIdAndUpdate({_id: blogId }, { $set: stringUpdates, $push: arrayUpdates }, { new: true })
-    return res.status(200).send({ status: true, data: updatedBlog })
+    try {
+        let blogId = req.params.blogId;
+        if (!blogId)
+            return res.status(404).send({ status: false, msg: "enter a blog ID" })
+        if (blogId.length > 24 || blogId.length < 24)
+            return res.status(400).send({ status: false, msg: "Invalid ID" })
+        let valid = await blogModel.findById(blogId)
+        if (!valid)
+            return res.status(404).send({ status: false, msg: "invalid blog ID" })
+        if (valid.isDeleted == true)
+            return res.status(404).send({ status: false, msg: "no such blog exists" })
+       
+        let updates = req.body
+        if (Object.keys(updates).length ==0)
+            return res.status(400).send({ status: false, msg: "send something to update" })
+        let stringUpdates = {
+            isPublished: true,
+            publishedAt: today
+        };
+        let arrayUpdates = {};
+        if (updates["title"])
+            stringUpdates["title"] = updates.title;
+        if (updates["body"])
+            stringUpdates["body"] = updates.body;
+        if (updates["tags"])
+            arrayUpdates["tags"] = updates.tags;
+        if (updates["subcategory"])
+            arrayUpdates["subcategory"] = updates.subcategory;
+
+        let updatedBlog = await blogModel.findByIdAndUpdate({ _id: blogId }, { $set: stringUpdates, $push: arrayUpdates }, { new: true })
+        return res.status(200).send({ status: true, data: updatedBlog })
     }
-    catch(error){
-        res.status(500).send({status:false,msg:error.message})
+    catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
     }
 }
 
 const deleteBlogsParam = async (req, res) => {
     try {
         let iDfromParams = req.params.blogId
-       if(iDfromParams.length>24 ||iDfromParams.length<24)
-      return res.status(400).send({ status: false, msg: "Invalid ID" })
-      
+        if (!iDfromParams)
+            return res.status(400).send({ status: false, msg: "pleas enter  ID" })
+        if (iDfromParams.length > 24 || iDfromParams.length < 24)
+        return res.status(400).send({ status: false, msg: "Invalid ID" })
+
         let data03 = await blogModel.findById(iDfromParams)
-        console.log(data03)
+      
         if (!data03)
-        res.status(400).send({ status: false, msg: "Invalid ID" })
+            res.status(400).send({ status: false, msg: "Invalid ID" })
         if (data03.isDeleted) {
-        return  res.status(404).send({ status: false, msg: "blog document doesn't exist" })
+            return res.status(404).send({ status: false, msg: "blog document doesn't exist" })
         }
-        let data04 = await blogModel.updateOne((data03), { $set: { isDeleted: true } })
-        return  res.status(200).send({ status: true, data: data04 })
+       
+         await blogModel.updateOne((data03), { $set: { isDeleted: true , deletedAt: today } })
+        return res.status(200).send()
     }
     catch (error) {
         res.status(500).send({ status: false, msg: error.message })
@@ -102,12 +122,18 @@ const deleteBlogsParam = async (req, res) => {
 
 const deleteBlogsQuery = async function (req, res) {
     try {
-        let data0 = req.query
-        const data1 = await blogModel.findOne(data0)
+        let data = req.query
+        if (Object.keys(data).length ==0)
+            return res.status(400).send({ status: false, msg: "please enter data to update" })
+        const data1 = await blogModel.findOne(data)
+        if (!data1)
+            return res.status(400).send({ status: false, msg: "invalid data" })
+            
         if (data1.isDeleted) {
             return res.status(404).send({ status: false, msg: "blog document doesn't exist" })
         }
-        const data2 = await blogModel.updateOne(data1, { $set: { isDeleted: true } })
+
+        const data2 = await blogModel.updateMany(data1, { $set: { isDeleted: true, deletedAt: today } })
         res.status(200).send({ status: true, data: data2 })
 
     } catch (error) {
@@ -115,8 +141,4 @@ const deleteBlogsQuery = async function (req, res) {
     }
 }
 
-module.exports.getBlogs = getBlogs;
-module.exports.createBlog = createBlog;
-module.exports.updateBlog = updateBlog;
-module.exports.deleteBlogsParam=deleteBlogsParam;
-module.exports.deleteBlogsQuery=deleteBlogsQuery;
+module.exports ={ getBlogs,createBlog,updateBlog,deleteBlogsParam,deleteBlogsQuery}
