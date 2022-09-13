a349408b8fccb92f508196b432108bc73db8d92b
aadityapatil12 <aadityapatil83089@gmail.com>
2022-09-13 16:53:11 +0530

    start project-2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,40 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const authorcontroller = require('../controllers/authorController')
-const blogcontroller = require('../controllers/blogController')
-const middleware = require("../middlewares/auth")
-
-
-
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 // ===========================================================
-//creaet author
-router.post("/authors", authorcontroller.createAuthor)
-
-//login
-router.post("/login", authorcontroller.login)
-
-//create blogs
-router.post("/blogs", middleware.authenticate, blogcontroller.createBlog)
-
-//find
-router.get("/blogs", middleware.authenticate, blogcontroller.getBlogs)
-
-//update
-router.put("/blogs/:blogId",middleware.authenticate, middleware.Authorise, blogcontroller.updateBlog)
-
-//delete
-router.delete("/blogs/:blogId",middleware.authenticate, middleware.Authorise, blogcontroller.deleteBlogsParam)
-
-//delete
-router.delete("/blogs", middleware.authenticate, middleware.deleteAuthorised, blogcontroller.deleteBlogsQuery)
-
-
-
-
 
 
 module.exports = router;
