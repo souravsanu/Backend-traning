0963f9c5b93d5ce0901585bb0a23bbc0602ca819
aadityapatil12 <aadityapatil83089@gmail.com>
2022-09-14 12:44:26 +0530

    create collage

--- src/controllers/collageController.js
+++ src/controllers/collageController.js
@@ -1,24 +1,33 @@
 const collageModel = require('../models/collageModel')
-const Validater = require('../validater/validater')
+let regexValidation = /^[a-zA-Z]+([\s][a-zA-Z]+)*$/;
+let regexlogoLink = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
 const createcollage = async function (req, res) {
     try {
         let data = req.body
-        if(!data) return res.status(400).send({ status: false, msg:"plzz give some data"})
-        const {name,fullName,logoLink} = data
+        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, msg: "plzz give some data" })
 
-        if(!isValid(name)) return res.status(400).send({ status: false, msg:"Enter collage name"})
+        const { name, fullName, logoLink } = data
 
-        if(!isValid(fullName)) return res.status(400).send({ status: false, msg:"Enter collage FullName "})
+        if (!name) return res.status(400).send({ status: false, msg: "Enter collage name" })
+        if (!fullName) return res.status(400).send({ status: false, msg: "Enter collage FullName " })
+        if (!logoLink) return res.status(400).send({ status: false, msg: "Enter collage logoLink" })
 
-        if(!isValid(logoLink)) return res.status(400).send({ status: false, msg:"Enter collage logoLink"})
+        if (typeof name !== "string" || name[0] == " " || name[name.length - 1] == " " || !name.match(regexValidation)) {
+            return res.status(400).send({ status: false, msg: "please enetr a valid name" })
+        }
+        if (typeof fullName !== "string" || fullName[0] == " " || fullName[fullName.length - 1] == " " || !fullName.match(regexValidation)) {
+            return res.status(400).send({ status: false, msg: "please enetr a valid fullName" })
+        }
+        if (typeof logoLink !== "string" || logoLink[0] == " " || logoLink[logoLink.length - 1] == " " || !logoLink.match(regexlogoLink)) {
+            return res.status(400).send({ status: false, msg: "please enetr a valid logoLink" })
+        }
 
-        let findname = await collageModel.findOne({ name : name })
+        let findname = await collageModel.findOne({ name: name })
         if (findname) {
             return res.status(400).send({ status: false, msg: "name already exsits" })
         }
-
-        let collage = await collageModel.create(data)
+         let collage = await collageModel.create(data)
         res.status(201).send({ status: true, Data: collage })
     } catch (error) {
         res.status(500).send({ status: false, msg: error.message })
