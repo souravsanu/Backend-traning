1d0b8e06057f5e222d516552646304e58f6d25e7
Manjusha003 <manjusharaut003@gmail.com>
2022-09-20 12:38:23 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const router=express.Router();
+const bodyParser = require('body-parser');
+const route=require("./route/routes")
+const app=express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://miniblogeproject:akash1234atlas@cluster0.lz7q3.mongodb.net/project/booksManagementGroup10",{
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+app.use("/",route)
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
