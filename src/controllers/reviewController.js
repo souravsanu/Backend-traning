b81fcb97e2c742cd88220ba533e531012ee30b94
Manjusha003 <manjusharaut003@gmail.com>
2022-09-22 12:19:05 +0530

    Update book by reviwId and BookId

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -98,5 +98,66 @@ const createReview = async function (req, res) {
             send({ status: false, msg: error.message })
     }
 }
+const updateReviews=async function(req,res){
+    try{
+    const bookParams=req.params.bookId;
+    const reviewParams=req.params.reviewId;
+    const reqbody=req.body;
+    const{review,rating,reviewedBy}=reqbody
 
-module.exports={createReview}
+    if(!bookParams) return res.send({msg:"bookId is not present"});
+    if(!mongoose.isValidObjectId(bookParams)) return res.send({msg:"bookId is not valid"});
+
+    if(!reviewParams)return res.send({msg:"reviewId is not present"})
+    if(!mongoose.isValidObjectId(reviewParams)) return res.send({msg:"reviewId is not valid"});
+
+    if(!Object.keys(reqbody)) return res.send({msg:"Please provide data into the request body"});
+    
+    if (Object.keys(reqbody).some(a => a == "review")) {
+        if (!review) return res.status(400).send({ status: false, msg: "review is required" });
+        if (!isNotEmpty(review)) return res.status(400).send({ msg: "review is empty" })
+        if (!isValidName(review)) return res.status(400).send({ msg: "Type of review must be string" });
+     
+    }
+
+    if (Object.keys(reqbody).some(a => a == "rating")) {
+        if (!rating) return res.status(400).send({ status: false, msg: "rating is required" });   
+        if (!isValidRating(rating)) return res.status(400).send({ msg: "Type of rating must be string" })
+        
+    }
+    if (Object.keys(reqbody).some(a => a == "reviewedBy")) {
+        if (!reviewedBy) return res.status(400).send({ status: false, msg: "reviewedBy is required" });
+        if (!isNotEmpty(reviewedBy)) return res.status(400).send({ msg: "reviewedBy is empty" })
+        if (!isValidName(reviewedBy)) return res.status(400).send({ msg: "Type of reviewedBy must be string" });
+
+    }
+    const findBook=await bookModel.findOne({_id:bookParams,isDeleted:false});
+    if(!findBook) return res.send({msg:"Book is not found"});
+
+    const findReview=await reviewModel.findOne({_id:reviewParams,isDeleted:false});
+    if(!findReview) return res.send("review is not present for this book");
+
+
+    if(findReview.bookId!=bookParams) return res.send({msg:"There is no review for this book"});
+
+
+ const updateReviewDetails = await reviewModel.
+ findOneAndUpdate({ _id: reviewParams },
+     {$set:{ review: review, rating: rating, reviewedBy: reviewedBy }},
+      { new: true }).select({createdAt:0,updatedAt:0,__v:0})
+
+      let upadtedBook = findBook.toObject();
+      upadtedBook['reviewsData'] = [updateReviewDetails];
+      return res.status(200).send({ status: true, message: "Successfully updated the review of the book.", data: upadtedBook })
+
+
+    }
+    catch (error) {
+        res.
+            status(500).
+            send({ status: false, msg: error.message })
+    }
+
+}
+
+module.exports={createReview,updateReviews}
