3433d1a0c1c2dc381bea6ff48102b4b6819cfad6
souravsanu <souravmondal01bscctis@gmail.com>
2022-09-27 21:59:02 +0530

    ncbhbv

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-const express=require("express");
-const mongoose=require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 
-const bodyParser = require('body-parser');
-const route=require("./route/routes")
-const app=express();
+const bodyParser = require("body-parser");
+const route = require("./route/routes");
+const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://ManjushaRaut:D1NNvookajCHUeKG@cluster0.3qd4bit.mongodb.net/booksManagementGroup10?retryWrites=true&w=majority",{
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-app.use("/",route)
+mongoose
+  .connect(
+    "mongodb+srv://ManjushaRaut:D1NNvookajCHUeKG@cluster0.3qd4bit.mongodb.net/booksManagementGroup10?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+app.use("/", route);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
