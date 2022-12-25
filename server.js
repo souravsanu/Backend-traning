406ad474e1838ac365c11c80050ccadd68d60d65
souravsanu <110589654+souravsanu@users.noreply.github.com>
2022-12-26 02:41:26 +0530

    server change
    some part

--- server.js
+++ server.js
@@ -20,13 +20,16 @@ io.on("connection", (socket) => {
   socket.on("new-user-joined", (username) => {
     users[socket.id] = username;
     socket.broadcast.emit("user-connected", username);
-    io.emit("users-list", users);
+    io.emit("user-list", users);
   });
 
   socket.on("disconnect", () => {
     socket.broadcast.emit("user-disconnected", (user = users[socket.id]));
     delete users[socket.id];
-    io.emit("users-list", users);
+    io.emit("user-list", users);
+  });
+  socket.on("message", (data) => {
+    socket.broadcast.emit("message", { user: data.user, msg: data.msg });
   });
 });
 
